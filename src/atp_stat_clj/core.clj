3e49b4ba49fe6b89987a0f8dd134df1e1305632e
maixianyu <maixianyu@qq.com>
2018-07-28 22:06:59 +0800

    filter successfully

--- src/atp_stat_clj/core.clj
+++ src/atp_stat_clj/core.clj
@@ -1,14 +1,23 @@
 (ns atp-stat-clj.core
   (:require [clojure-csv.core :as csv])
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
 
 
-(with-open [r (io/reader "resources/csv/3_match_stats/match_stats_2018_0.csv")]
-  (def atp-csv (csv/parse-csv r)))
+(def freader (io/reader "resources/csv/3_match_stats/match_stats_2018_0.csv"))
 
-(println atp-csv)
+(def atp-csv (csv/parse-csv freader))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+atp-csv
+
+(str/includes? "0" "0")
+
+(map (fn [x] x) (seq [1 2 3]))
+
+(def res-csv
+  (filter (fn [x]
+            (str/includes? (get x 0) "0"))
+          atp-csv))
+
+(println res-csv)
+
