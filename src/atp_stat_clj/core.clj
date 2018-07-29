13fc3f7b0b185ac661ff70d374295c9f9cd80fd9
maixianyu <maixianyu@qq.com>
2018-07-29 16:45:55 +0800

    grep func is finished

--- src/atp_stat_clj/core.clj
+++ src/atp_stat_clj/core.clj
@@ -3,21 +3,75 @@
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as str]))
 
+; config file path
+(def dir "resources/csv/2_match_scores/")
+(def file-list "file.list")
+(def item-map-file "match_scores_column_titles.txt")
 
-(def freader (io/reader "resources/csv/3_match_stats/match_stats_2018_0.csv"))
+(def item-map
+  (zipmap
+    (line-seq (io/reader (str dir item-map-file)))
+    (range (count (line-seq (io/reader (str dir item-map-file)))))
+    ))
 
-(def atp-csv (csv/parse-csv freader))
+item-map
 
-atp-csv
+(defn file-path-list [dir-path file-list]
+  (map #(str dir-path %)
+    (line-seq
+      (io/reader (str dir-path file-list)))))
 
-(str/includes? "0" "0")
+; func to read a csv file
+(defn read-csvfile [s]
+  (csv/parse-csv (io/reader s)))
 
-(map (fn [x] x) (seq [1 2 3]))
+; func to read many csv file
+(defn read-and-merge-csv-file [dir-path file-list]
+  (reduce
+    concat
+    (map read-csvfile (file-path-list dir-path file-list))))
 
-(def res-csv
-  (filter (fn [x]
-            (str/includes? (get x 0) "0"))
-          atp-csv))
+; get all-stats
+(def all-stats (read-and-merge-csv-file dir file-list))
 
-(println res-csv)
+(count all-stats)
 
+(defn grep-item [stat-seq item-name item-map pred]
+  (filter
+    #(pred (get % (get item-map item-name)))
+    stat-seq))
+
+(get item-map "winner_name")
+
+
+(def win_count_roger
+  (count (grep-item all-stats
+             "winner_name"
+             item-map
+             #(str/includes? % "Roger Federer"))))
+
+(def lose_count_roger
+  (count (grep-item all-stats
+             "loser_name"
+             item-map
+             #(str/includes? % "Roger Federer"))))
+
+(def win_count_dj
+  (count (grep-item all-stats
+             "winner_name"
+             item-map
+             #(str/includes? % "Novak Djokovic"))))
+
+(def lose_count_dj
+  (count (grep-item all-stats
+             "loser_name"
+             item-map
+             #(str/includes? % "Novak Djokovic"))))
+
+; 81.9%
+(/ win_count_roger (+ win_count_roger lose_count_roger))
+win_count_dj
+lose_count_dj
+
+; 83.2%
+(/ win_count_dj (+ lose_count_dj win_count_dj))
